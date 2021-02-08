457cd99c5bb1ac0d412aa8cc27e19c43f484491c
Ainur <zaripow.aynur@yandex.ru>
2021-02-08 12:10:38 +0300

    Add DAL

---
+++ ReactCore/ReactCoreApiApp/Controllers/AddressesController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using ReactCoreApiApp;
+using ReactCoreApiApp.Filters;
+using ReactCoreApiApp.Repository;
+
+namespace ReactCoreApiApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AddressesController : BaseApiController<Addresses>
+    {
+        public AddressesController(IGenericRepository<Addresses> repository) : base(repository)
+        {
+        }
+
+        // GET: api/Addresses
+        [HttpGet]
+        public override IList<Addresses> Get()
+        {
+            return base.Get();
+        }
+
+        // GET: api/Addresses/5
+        [HttpGet("{id}")]
+        public override IActionResult Get(int id)
+        {
+            return base.Get(id);
+        }
+
+        //// PUT: api/Addresses/5
+        [HttpPut("{id}")]
+        public override IActionResult Put(Addresses addresses)
+        {
+            return base.Put(addresses);
+        }
+
+        //// POST: api/Addresses
+        [HttpPost]
+        public override IActionResult Post(Addresses addresses)
+        {
+            return base.Post(addresses);
+        }
+
+        //// DELETE: api/Addresses/5
+        [HttpDelete("{id}")]
+        public override IActionResult Delete(Addresses addresses)
+        {
+            return base.Delete(addresses);
+        }
+
+        // DELETE: api/Addresses/filter={,,,}
+        [HttpDelete]
+        public override IActionResult Delete(string filter)
+        {
+            return base.Delete(filter);
+        }
+
+    }
+}
---
+++ ReactCore/ReactCoreApiApp/Controllers/Common/IBaseApiController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace ReactCoreApiApp.Controllers.Common
+{
+    interface IBaseApiController<T> where T : class
+    {
+        IList<T> Get();
+        IActionResult Get(int id);
+        IActionResult Put(T item);
+        IActionResult Post(T item);
+        IActionResult Delete(T item);
+        IActionResult Delete(string filter);
+    }
+}
